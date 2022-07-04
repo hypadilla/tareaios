d5d0962b8faa74bfa4ab918dd3ec1943383a3a62
Merge <merge@MacBook-Pro-de-Merge.local>
2022-07-04 15:00:26 -0500

    hola

---
+++ VistaCodigoTarea/Vistas/ServiciosFinancierios/Cells/CeldaTablaBienestar2xCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CeldaTablaBienestar2xCollectionViewCell.swift
+//  VistaCodigoTarea
+//
+//  Created by Merge on 4/07/22.
+//
+
+import UIKit
+
+class CeldaTablaBienestar2xCollectionViewCell: UICollectionViewCell {
+    
+}
